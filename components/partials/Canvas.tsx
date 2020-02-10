4f89e3b99b825cc38ddb1729901b78791eb24f6a
shuta13 <hollystarsun@gmail.com>
2020-02-10 17:57:42 +0900

    chore: added three

--- components/partials/Canvas.tsx
+++ components/partials/Canvas.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import {
+  WebGLRenderer,
+  Scene, 
+  PerspectiveCamera,
+} from 'three'
+import useGetWindowSize from '../hooks/useGetWindowSize'
 
 import './Canvas.scss'
 
 const Canvas: React.FC = () => {
+  const { width, height } = useGetWindowSize()
+  const onCanvasLoaded = (canvas: HTMLCanvasElement) => {
+    if (!canvas) {
+      return
+    }
+
+    // init scene
+    const scene = new Scene()
+    const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000)
+    const renderer = new WebGLRenderer({ canvas: canvas, antialias: true })
+    renderer.setClearColor('#1d1d1d')
+    renderer.setSize(width, height)
+
+    // render scene
+    renderer.render(scene, camera)
+  }
   return (
-    <div className="CanvasWrap">This is Canvas</div>
+    <div className="CanvasWrap">
+      <canvas ref={onCanvasLoaded} />
+    </div>
   )
 }
 
